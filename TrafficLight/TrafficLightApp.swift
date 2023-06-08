974594b960898448fbfee79f1ed282bbaf1f17fd
Julia Kazarina <120021628+kazarinaJu@users.noreply.github.com>
2023-06-08 19:25:30 +0500

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Юлия Ястребова on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
